63387924ba72a4b78f56c8c893f2951b445afa6a
Daewon Min <damin@c10r3s13.42seoul.kr>
2024-05-28 21:32:05 +0900

    terminal attribute save & restore

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:02:22 by damin             #+#    #+#             */
-/*   Updated: 2024/05/28 18:05:31 by damin            ###   ########.fr       */
+/*   Updated: 2024/05/28 19:58:22 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,45 @@ void	handler(int signo)
 	rl_replace_line("", 1);
 	rl_redisplay();
 }
+void	set_signal()
+{
+	signal(SIGINT, handler);
+	signal(SIGQUIT, SIG_IGN);
+}
+
+void	save_termi_attr(struct termios *old_term, struct termios *new_term)
+{
+	tcgetattr(STDIN_FILENO, old_term);
+    tcgetattr(STDIN_FILENO, new_term);
+    new_term->c_lflag &= ~(ICANON | ECHOCTL);
+    tcsetattr(STDIN_FILENO, TCSANOW, new_term);
+}
+
+void	restore_termi_attr(struct termios *old_term)
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term);
+}
 
 int	main(void)
 {
-	char			*input;
-	struct termios	term;
+	char			*line;
+	struct termios	old_term;
+	struct termios	new_term;
 
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~(ICANON | ECHOCTL);
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-	signal(SIGINT, handler);
-	signal(SIGQUIT, SIG_IGN);
+	save_termi_attr(&old_term, &new_term);
     while(1)
 	{
-        input = readline("Nutshell $ ");
-        if (!input)
+        line = readline("Nutshell $ ");
+        if (!line)
+		{
+
             break;
-        add_history(input);
-        free(input);
+		}
+        add_history(line);
+        free(line);
     }
+	restore_termi_attr(&old_term);
 	return (0);
 }
+
+//gcc minishell.c minishell.h -lreadline -L${HOME}/.brew/opt/readline/lib
