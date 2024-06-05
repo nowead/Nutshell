14ab5ea1cac92c2c90924b0b74ce59bd4aa4dc1f
Daewon Min <damin@c10r3s13.42seoul.kr>
2024-06-05 17:04:36 +0900

    readline history error fix

--- sources/parser_main.c
+++ sources/parser_main.c
@@ -6,7 +6,7 @@
 /*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 22:13:36 by seonseo           #+#    #+#             */
-/*   Updated: 2024/06/04 19:43:29 by damin            ###   ########.fr       */
+/*   Updated: 2024/06/05 12:56:12 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ int main()
 	t_ast	*ast;
 
 	// atexit(leak_check);
-	ast = parse("<< EOF < infile ls < infile1 > outfile | cat");
+	//ast = parse("<< EOF < infile ls < infile1 > outfile | cat");
 	prompt();
-	if (ast != NULL)
-	{
-		ft_printf("\n\n");
-		print_ast(ast->root, 0);
-		ft_printf("\n\n");
-		tokenlist_clear(ast->tokenlist);
-		clear_ast(ast->root);
-		free(ast);
-	}
+	// if (ast != NULL)
+	// {
+	// 	ft_printf("\n\n");
+	// 	print_ast(ast->root, 0);
+	// 	ft_printf("\n\n");
+	// 	tokenlist_clear(ast->tokenlist);
+	// 	clear_ast(ast->root);
+	// 	free(ast);
+	// }
     return (0);
 }
