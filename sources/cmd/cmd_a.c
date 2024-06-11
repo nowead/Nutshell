69cb010753fb4bd24d65e1effaaa1a61139f76af
Daewon Min <damin@c10r3s13.42seoul.kr>
2024-06-11 21:27:43 +0900

    cmd and_or ~ pipe_sequence

--- sources/cmd/cmd_a.c
+++ sources/cmd/cmd_a.c
@@ -6,27 +6,15 @@
 /*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:27:34 by damin             #+#    #+#             */
-/*   Updated: 2024/06/07 17:00:24 by damin            ###   ########.fr       */
+/*   Updated: 2024/06/11 21:25:18 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	e_and_or()
-{
-	e_pipe_sequence();
-	e_and_or_();
-}
-
-int e_and_or_()
+int simple_cmd()
 {
 	
-}
-
-int e_pipe_sequence()
-{
-	e_cmd();
-	e_pipe_sequence_();
 }
 
 int e_pipe_sequence_()
@@ -35,10 +23,54 @@ int e_pipe_sequence_()
 	e_pipe_sequence_();
 }
 
-int simple_cmd()
+int e_pipe_sequence(t_ast_node *node)
 {
-	
+	int	i;
+
+	if (node == NULL)
+		return (1);
+	i = 0;
+	if (node->child && node->child_num == 2)
+		return (e_pipe_sequence_(node->child[i]));
+	return (e_cmd(node->child[i++]));
 }
+
+int e_and_or_(t_ast_node *node, int ret_pipe_sequence)
+{
+	int	i;
+	int	ret_pipe_sequence_;
+
+	if (node == NULL)
+		return (1);
+	ret_pipe_sequence_ = 0;
+	i = 0;
+	if ((node->token->type == AND_IF && ret_pipe_sequence == 1) ||\
+	 (node->token->type == OR_IF && ret_pipe_sequence == 0))
+	{
+		if (node->child && i < node->child_num)
+			ret_pipe_sequence_ = e_pipe_sequence(node->child[i++]);
+		if (node->child && i < node->child_num)
+			reaturn (e_and_or_(node->child[i], ret_pipe_sequence_));
+	}
+	return (1);
+}
+
+int	e_and_or(t_ast_node *node)
+{
+	int	i;
+	int	ret_pipe_sequence;
+
+	if (node == NULL)
+		return (1);
+	ret_pipe_sequence = 0;
+	i = 0;
+	if (node->child && i < node->child_num)
+		ret_pipe_sequence = e_pipe_sequence(node->child[i++]);
+	if (ret_pipe_sequence != 0 && i < node->child_num)
+		return (e_and_or_(node->child[i], ret_pipe_sequence));
+	return (ret_pipe_sequence);
+}
+
 	// simple_command   : cmd_prefix cmd_word cmd_suffix
 	//              | cmd_prefix
 	//              | cmd_name cmd_suffix
--- sources/cmd/ctrl_cmd.c
+++ sources/cmd/ctrl_cmd.c
@@ -6,7 +6,7 @@
 /*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:57:38 by damin             #+#    #+#             */
-/*   Updated: 2024/06/10 20:57:55 by damin            ###   ########.fr       */
+/*   Updated: 2024/06/11 18:39:41 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,43 @@ void	exec(t_ast_node *node)
 	// printf("Symbol: %s\n", get_symbol_type_string(node->sym));
 	// printf("  Token Type: %s\n", get_token_type_string(node->token->type));
 	// printf("  Token String: %s\n", node->token->str);
-	if (node->sym == HERE_END)
-		e_io_here(node->token->str);
+	// if (node->sym == IO_HERE)
+	// 	e_io_here(node->parent->child[i + 1]->token->str);
+	// 	//node->token->str
 }
+
+// void	exec_cmd(t_ast_node *node)
+// {
+// 	size_t	i;
+
+// 	if (node == NULL) 
+// 		return ;
+// 	//printf("Symbol: %s\n", get_symbol_type_string(node->sym));
+// 	// printf("Sibling num: %d\n", node->sibling_index);
+// 	if (node->token)
+// 		exec(node);
+// 	i = 0;
+// 	while (node->child && i < node->child_num)
+// 	{
+// 		exec_cmd(node->child[i]);
+// 		i++;
+// 	}
+// }
 
 void	exec_cmd(t_ast_node *node)
 {
-	size_t	i;
-
 	if (node == NULL) 
 		return ;
 	//printf("Symbol: %s\n", get_symbol_type_string(node->sym));
-	if (node->token)
-		exec(node);
-	i = 0;
-	while (node->child && i < node->child_num)
-	{
-		exec_cmd(node->child[i]);
-		i++;
-	}
+	// printf("Sibling num: %d\n", node->sibling_index);
+	//e_and_or(node);
 }
 
 void	ctrl_cmd(t_ast *ast)
 {
-	// ft_printf("\n\n");
-	// print_ast(ast->root, 0);
-	// ft_printf("\n\n");
+	ft_printf("\n\n");
+	print_ast(ast->root, 0);
+	ft_printf("\n\n");
 	// print_tokenlist(ast->tokenlist);
 	// ft_printf("\n\n");
 	exec_cmd(ast->root);
