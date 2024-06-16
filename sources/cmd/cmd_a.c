9b63325251dc79ffdac068059ea781b7ff658509
Daewon Min <damin@c10r3s13.42seoul.kr>
2024-06-16 17:34:09 +0900

    cmd_suffix option

--- sources/cmd/cmd_a.c
+++ sources/cmd/cmd_a.c
@@ -6,11 +6,34 @@
 /*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:27:34 by damin             #+#    #+#             */
-/*   Updated: 2024/06/14 17:31:26 by damin            ###   ########.fr       */
+/*   Updated: 2024/06/16 17:31:32 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	*ft_realloc(void *ptr, size_t new_size)
+{
+	void	*new_ptr;
+	size_t	old_size;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		return malloc(new_size);
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	old_size = ft_strlen(ptr);
+	if (new_size < old_size)
+		old_size = new_size;
+    ft_memcpy(new_ptr, ptr, old_size);
+    free(ptr);
+	return (new_ptr);
+}
 
 int	e_assignment_word(t_ast_node *node)
 {
@@ -38,7 +61,7 @@ int	e_io_redirect(t_ast_node *node)
 
 int	e_cmd_suffix(t_ast_node *node, char *option)
 {
-	char	*new_option;
+	size_t	dst_size;
 
 	if (node == NULL)
 		return (0);
@@ -51,11 +74,15 @@ int	e_cmd_suffix(t_ast_node *node, char *option)
 			if (e_cmd_suffix(node->child[1], option) == -1)
 				return (-1);
 		}
-		else if (node->child[0] && node->child[0]->sym == WORD)
+		else if (node->child[0] && node->child[0]->sym == TERMINAL)
 		{
-			new_option = ft_strjoin(option, node->token->str);
-			free(option);
-			option = new_option;
+			dst_size = ft_strlen(option) + ft_strlen(node->child[0]->token->str) + 2;
+			option = (char *)ft_realloc(option, dst_size);
+			if (option == NULL)
+				return (-1);
+			ft_strlcat(option, " ", ft_strlen(option) + 2);
+			ft_strlcat(option, node->child[0]->token->str, dst_size);
+			// new_option = ft_strjoin(option, node->child[0]->token->str);
 			if (e_cmd_suffix(node->child[1], option) == -1)
 				return (-1);
 		}
@@ -65,6 +92,7 @@ int	e_cmd_suffix(t_ast_node *node, char *option)
 
 int	e_cmd_name(t_ast_node *node, char *option)
 {
+	printf("[cmd_name:%s] : [cmd_options:%s]\n", node->token->str, option);
 	return (-1);
 }
 
@@ -138,20 +166,26 @@ int	first_cmd(t_ast_node *node)
 	ft_printf("this is first_cmd\n");
 	if (node->child)
 	{
-		if (node->child_num == 1)
+		if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
 			return (e_simple_cmd(node->child[0]));
 		else
-		{
- 			e_redirect_list(node->child[1]);
-			return (e_subshell(node->child[0]));
-		}
+ 			if (e_redirect_list(node->child[1]) != -1)
+				return (e_subshell(node->child[0]));
 	}
-	return (0);
+	return (-1);
 }
 
 int	middle_cmd(t_ast_node *node)
 {
 	ft_printf("this is middle_cmd\n");
+	if (node->child)
+	{
+		if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
+			return (e_simple_cmd(node->child[0]));
+		else
+ 			if (e_redirect_list(node->child[1]) != -1)
+				return (e_subshell(node->child[0]));
+	}
 	return (0);
 }
 
@@ -160,10 +194,27 @@ int	last_cmd(t_ast_node *node, int depth)
 	if (depth == 0) // 단일 명령어
 	{
 		ft_printf("this is only one cmd\n");
-		return (e_simple_cmd(node->child[0]));
+		if (node->child)
+		{
+			if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
+				return (e_simple_cmd(node->child[0]));
+			else
+				if (e_redirect_list(node->child[1]) != -1)
+					return (e_subshell(node->child[0]));
+		}
 	}
 	else
+	{
 		ft_printf("this is last cmd\n");
+		if (node->child)
+		{
+			if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
+				return (e_simple_cmd(node->child[0]));
+			else
+				if (e_redirect_list(node->child[1]) != -1)
+					return (e_subshell(node->child[0]));
+		}
+	}
 	return (0);
 }
 
