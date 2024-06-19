55266967fd26d1ea95e3bf7fa2556dde3364a974
seonseo <seonseo@student.42seoul.kr>
2024-06-19 22:52:35 +0900

    Refactor cmd_a.c
    Change name of variables (t_ast_node *)'node' to 'curr'
    
    Refactor e_pipe_sequence
    - delete 'ret' variable since it's not used
    
    Refactor first_cmd, middle_cmd, last_cmd
    
    Implement exec_cmd() function
    
    Implement is_there_pipe() in e_pipe_sequence()

--- sources/cmd/cmd_a.c
+++ sources/cmd/cmd_a.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_a.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
+/*   By: seonseo <seonseo@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:27:34 by damin             #+#    #+#             */
-/*   Updated: 2024/06/19 16:12:48 by damin            ###   ########.fr       */
+/*   Updated: 2024/06/19 22:45:57 by seonseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ void	*ft_realloc(void *ptr, size_t new_size)
 		return (NULL);
 	}
 	if (ptr == NULL)
-		return malloc(new_size);
+		return (malloc(new_size));
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
-	old_size = ft_strlen(ptr);
+	old_size = ft_strlen(ptr) + 1;
 	if (new_size < old_size)
 		old_size = new_size;
     ft_memcpy(new_ptr, ptr, old_size);
@@ -35,110 +35,110 @@ void	*ft_realloc(void *ptr, size_t new_size)
 	return (new_ptr);
 }
 
-int	e_assignment_word(t_ast_node *node)
+int	e_assignment_word(t_ast_node *curr)
 {
 	return (0);
 }
 
-int	e_io_redirect(t_ast_node *node)
+int	e_io_redirect(t_ast_node *curr)
 {
-	if (node->child[0]->sym == IO_FILE)
-		return (e_io_file(node->child[0]));
-	else if (node->child[0]->sym == IO_HERE)
-		return (e_io_here(node->child[0]));
+	if (curr->child[0]->sym == IO_FILE)
+		return (e_io_file(curr->child[0]));
+	else if (curr->child[0]->sym == IO_HERE)
+		return (e_io_here(curr->child[0]));
 	return (-1);
 }
 
-int	e_redirect_list(t_ast_node *node)
+int	e_redirect_list(t_ast_node *curr)
 {
 	int	ret;
 
 	ret = -1;
-	if (node == NULL)
+	if (curr == NULL)
 		return (0);
-	if (node->child)
+	if (curr->child)
 	{
-		if (node->child[0])
-			ret = e_io_redirect(node->child[0]);
-		if (node->child[1] && ret != -1)
-			return (e_redirect_list(node->child[1]));
+		if (curr->child[0])
+			ret = e_io_redirect(curr->child[0]);
+		if (curr->child[1] && ret != -1)
+			return (e_redirect_list(curr->child[1]));
 	}
 	return (ret);
 }
 
-int	e_cmd_suffix(t_ast_node *node, char **option)
+int	e_cmd_suffix(t_ast_node *curr, char **option)
 {
 	size_t	dst_size;
 
-	if (node != NULL && node->child)
+	if (curr != NULL && curr->child)
 	{
-		if (node->child[0] && node->child[0]->sym == IO_REDIRECT)
+		if (curr->child[0] && curr->child[0]->sym == IO_REDIRECT)
 		{
-			if (e_io_redirect(node->child[0]) == -1)
+			if (e_io_redirect(curr->child[0]) == -1)
 				return (-1);
-			if (e_cmd_suffix(node->child[1], option) == -1)
+			if (e_cmd_suffix(curr->child[1], option) == -1)
 				return (-1);
 		}
-		else if (node->child[0] && node->child[0]->sym == TERMINAL)
+		else if (curr->child[0] && curr->child[0]->sym == TERMINAL)
 		{
-			dst_size = ft_strlen(*option) + ft_strlen(node->child[0]->token->str) + 2;
+			dst_size = ft_strlen(*option) + ft_strlen(curr->child[0]->token->str) + 2;
 			*option = (char *)ft_realloc(*option, dst_size);
 			if (*option == NULL)
 				return (-1);
 			ft_strlcat(*option, " ", ft_strlen(*option) + 2);
-			ft_strlcat(*option, node->child[0]->token->str, dst_size);
-			if (e_cmd_suffix(node->child[1], option) == -1)
+			ft_strlcat(*option, curr->child[0]->token->str, dst_size);
+			if (e_cmd_suffix(curr->child[1], option) == -1)
 				return (-1);
 		}
 	}
 	return (0);
 }
 
-int	e_cmd_name(t_ast_node *node, char **option)
+int	e_cmd_name(t_ast_node *curr, char **option)
 {
 	char	**options;
 
-	printf("[cmd_name:%s] : [cmd_options:%s]\n", node->token->str, *option);
+	printf("[cmd_name:%s] : [cmd_options:%s]\n", curr->token->str, *option);
 	options = ft_split(*option, ' ');
 	if (options == NULL)
 		return (-1);
 	return (0);
 }
 
-int	e_cmd_prefix_(t_ast_node *node)
+int	e_cmd_prefix_(t_ast_node *curr)
 {
 	int	ret;
 
 	ret  = -1;
-	if (node == NULL)
+	if (curr == NULL)
 		return (0);
-	if (node->child)
+	if (curr->child)
 	{
-		if (node->child[0] && node->child[0]->sym == IO_REDIRECT)
-			ret = e_io_redirect(node->child[0]);
-		else if (node->child[0] && node->child[0]->sym == ASSIGNMENT_WORD)
-			ret = e_assignment_word(node->child[0]);
-		if (node->child[1] && ret != -1)
-			return (e_cmd_prefix_(node->child[1]));
+		if (curr->child[0] && curr->child[0]->sym == IO_REDIRECT)
+			ret = e_io_redirect(curr->child[0]);
+		else if (curr->child[0] && curr->child[0]->sym == ASSIGNMENT_WORD)
+			ret = e_assignment_word(curr->child[0]);
+		if (curr->child[1] && ret != -1)
+			return (e_cmd_prefix_(curr->child[1]));
 	}
 	return (0);
 }
 
-int	e_cmd_prefix(t_ast_node *node)
+int	e_cmd_prefix(t_ast_node *curr)
 {
 	int	ret;
 
 	ret = -1;
-	if (node->child[0] && node->child[0]->sym == IO_REDIRECT)
-		ret = e_io_redirect(node->child[0]);
-	else if (node->child[0] && node->child[0]->sym == ASSIGNMENT_WORD)
-		ret = e_assignment_word(node->child[0]);
-	if (node->child[1] && ret != -1)
-		return (e_cmd_prefix_(node->child[1]));
+	if (curr->child[0] && curr->child[0]->sym == IO_REDIRECT)
+		ret = e_io_redirect(curr->child[0]);
+	else if (curr->child[0] && curr->child[0]->sym == ASSIGNMENT_WORD)
+		ret = e_assignment_word(curr->child[0]);
+	if (curr->child[1] && ret != -1)
+		return (e_cmd_prefix_(curr->child[1]));
 	return (ret);
 }
 
-int e_simple_cmd(t_ast_node *node)
+int e_simple_cmd(t_ast_node *curr)
 {
 	char	**option;
 	int		ret;
@@ -147,23 +147,23 @@ int e_simple_cmd(t_ast_node *node)
 	option = (char **)malloc(sizeof(char *));
 	if (option == NULL)
 		return (-1);
-	*option = (char *)calloc(1, sizeof(char));
+	*option = (char *)ft_calloc(1, sizeof(char));
 	if (*option == NULL)
 		return (-1);
 	if (option == NULL)
 		return (-1);
-	if (node->child[0]->sym == CMD_PREFIX && node->child[1] == NULL)
-		ret = e_cmd_prefix(node->child[0]);
-	else if(node->child[0]->sym == CMD_NAME)
+	if (curr->child[0]->sym == CMD_PREFIX && curr->child[1] == NULL)
+		ret = e_cmd_prefix(curr->child[0]);
+	else if(curr->child[0]->sym == CMD_NAME)
 	{
-		if (e_cmd_suffix(node->child[1], option) != -1)
-			ret = (e_cmd_name(node->child[0], option));
+		if (e_cmd_suffix(curr->child[1], option) != -1)
+			ret = (e_cmd_name(curr->child[0], option));
 	}
 	else
 	{
-		if (e_cmd_prefix(node->child[0]) != -1)
-			if (e_cmd_suffix(node->child[2], option) != -1)
-				ret = e_cmd_name(node->child[1], option);
+		if (e_cmd_prefix(curr->child[0]) != -1)
+			if (e_cmd_suffix(curr->child[2], option) != -1)
+				ret = e_cmd_name(curr->child[1], option);
 	}
 	free(*option);
 	free(option);
@@ -175,7 +175,16 @@ int	e_subshell(t_ast_node *node)
 	return (0);
 }
 
-int	first_cmd(t_ast_node *node, int fd[3])
+int	exec_cmd(t_ast_node *curr)
+{
+	if (curr->child[0]->sym == SIMPLE_COMMAND)
+		return (e_simple_cmd(curr->child[0]));
+	else
+		if (e_redirect_list(curr->child[1]) != -1)
+			return (e_subshell(curr->child[0]));
+}
+
+int	first_cmd(t_ast_node *curr, int fd[3])
 {
 	pid_t	pid;
 
@@ -191,25 +200,18 @@ int	first_cmd(t_ast_node *node, int fd[3])
 			err_ctrl("dup2 error ", 1, EXIT_FAILURE);
 		if (close(fd[0]) == -1 || close(fd[1]) == -1)
 			err_ctrl("close error ", 1, EXIT_FAILURE);
-		if (node->child)
-		{
-			if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
-				return (e_simple_cmd(node->child[0]));
-			else
-				if (e_redirect_list(node->child[1]) != -1)
-					return (e_subshell(node->child[0]));
-		}
+		return (exec_cmd(curr));
 	}
 	return (0);
 }
 
-int	middle_cmd(t_ast_node *node, int fd[3])
+int	middle_cmd(t_ast_node *curr, int fd[3])
 {
 	pid_t	pid;
 
-	fd[2] = fd[0];
-	if (close(fd[0]) == -1 || close(fd[1]) == -1)
+	if (close(fd[1]) == -1) // fd[0] shouldn't be closed
 		return (-1);
+	fd[2] = fd[0];
 	if (pipe(fd) == -1)
 		return (-1);
 	pid = fork();
@@ -226,132 +228,108 @@ int	middle_cmd(t_ast_node *node, int fd[3])
 			err_ctrl("dup2 error ", 1, EXIT_FAILURE);
 		if (dup2(fd[1], STDOUT_FILENO) == -1)
 			err_ctrl("dup2 error ", 1, EXIT_FAILURE);
-		if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1)
-			err_ctrl("close error ", 1, EXIT_FAILURE);
-		if (node->child)
-		{
-			if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
-				return (e_simple_cmd(node->child[0]));
-			else
-				if (e_redirect_list(node->child[1]) != -1)
-					return (e_subshell(node->child[0]));
-		}
+		return (exec_cmd(curr));
 	}
 	if (close(fd[2]) == -1)
-		err_ctrl("close error ", 1, EXIT_FAILURE);
+		return (-1);
 	return (0);
 }
 
-int	last_cmd(t_ast_node *node, int depth, int fd[3])
+int	last_cmd(t_ast_node *curr, int fd[3])
 {
 	pid_t	pid;
 
-	if (depth == 0) // 단일 명령어
+	if (close(fd[1]) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == -1)
 	{
-		pid = fork();
-		if (pid == -1)
-		{
-			if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1)
-				return (-1);
-			return (-1);
-		}
-		if (pid == 0)
-		{
-			ft_printf("this is only one cmd\n");
-			if (node->child)
-			{
-				if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
-					return (e_simple_cmd(node->child[0]));
-				else
-					if (e_redirect_list(node->child[1]) != -1)
-						return (e_subshell(node->child[0]));
-			}
-		}
+		close(fd[0]); // fd[1], fd[2] are already closed
+		return (-1);
 	}
-	else
+	if (pid == 0)
 	{
-		if (dup2(fd[0], fd[2]) == -1)
+		ft_printf("this is last cmd\n");
+		if (dup2(fd[0], STDIN_FILENO) == -1)
 			err_ctrl("dup2 error ", 1, EXIT_FAILURE);
-		pid = fork();
-		if (pid == -1)
-		{
-			if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1)
-				return (-1);
-			return (-1);
-		}
-		if (pid == 0)
-		{
-			ft_printf("this is last cmd\n");
-			if (dup2(fd[2], STDIN_FILENO) == -1)
-				err_ctrl("dup2 error ", 1, EXIT_FAILURE);
-			if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1)
-				err_ctrl("close error ", 1, EXIT_FAILURE);
-			if (node->child)
-			{
-				if (node->child[0] && node->child[0]->sym == SIMPLE_COMMAND)
-					return (e_simple_cmd(node->child[0]));
-				else
-					if (e_redirect_list(node->child[1]) != -1)
-						return (e_subshell(node->child[0]));
-			}
-		}
+		return (exec_cmd(curr));
 	}
-	if (close(fd[0]) == -1 || close(fd[1]) == -1 || close(fd[2]) == -1)
+	if (close(fd[0]) == -1)
 		return (-1);
 	return (0);
 }
 
-int	e_pipe_sequence(t_ast_node *node)
+int	single_cmd(t_ast_node *curr)
 {
-	t_ast_node	*curr;
-	int			fd[3];
-	int			ret;
+	pid_t	pid;
 
-	ret = 0;
-	curr = node;
-	if (curr->child[1]->child == NULL || curr->child[1]->child[0] == NULL)
-		return (last_cmd(curr->child[0], 0, fd));
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	if (pid == 0)
+	{
+		ft_printf("this is only one command\n");
+		return (exec_cmd(curr));
+	}
+	return (0);
+}
+
+int	is_there_pipe(t_ast_node *curr)
+{
+	if (curr->child[1]->child && curr->child[1]->child[0])
+		return (1);
+	else
+		return (0);
+}
+
+int	e_pipe_sequence(t_ast_node *curr)
+{
+	int			fd[3];
+
+	if (!is_there_pipe(curr))
+		return (single_cmd(curr->child[0]));
+	
 	if (first_cmd(curr->child[0], fd) == -1)
 		return (-1);
 	curr = curr->child[1];
-	while (curr->child[1]->child && curr->child[1]->child[0])
+	while (is_there_pipe(curr))
 	{
 		if (middle_cmd(curr->child[0], fd) == -1)
 			return (-1);
 		curr = curr->child[1];
 	}
-	return (last_cmd(curr->child[0], 1, fd));
+	return (last_cmd(curr->child[0], fd));
 }
 
-int e_and_or_(t_ast_node *node, int ret_pipe_sequence)
+int e_and_or_(t_ast_node *curr, int ret_pipe_sequence)
 {
 	int	ret_new_pipe_sequence;
 
-	if (node == NULL)
+	if (curr == NULL)
 		return (ret_pipe_sequence);
 	ret_new_pipe_sequence = 0;
-	if (node->token && \
-	((node->token->type == AND_IF && ret_pipe_sequence == 0) ||\
-	 (node->token->type == OR_IF && ret_pipe_sequence == -1)))
+	if (curr->token && \
+	((curr->token->type == AND_IF && ret_pipe_sequence == 0) ||\
+	 (curr->token->type == OR_IF && ret_pipe_sequence == -1)))
 	{
-		if (node->child)
+		if (curr->child)
 		{
-			ret_new_pipe_sequence = e_pipe_sequence(node->child[0]);
-			return (e_and_or_(node->child[1], ret_new_pipe_sequence));
+			ret_new_pipe_sequence = e_pipe_sequence(curr->child[0]);
+			return (e_and_or_(curr->child[1], ret_new_pipe_sequence));
 		}
 	}
 	return (ret_pipe_sequence);
 }
 
-int	e_and_or(t_ast_node *node)
+int	e_and_or(t_ast_node *curr)
 {
 	int	ret_pipe_sequence;
 
 	ret_pipe_sequence= -1;
-	if (node->child)
+	if (curr->child)
 	{
-		ret_pipe_sequence = e_pipe_sequence(node->child[0]);
-		return (e_and_or_(node->child[1], ret_pipe_sequence));
+		ret_pipe_sequence = e_pipe_sequence(curr->child[0]);
+		return (e_and_or_(curr->child[1], ret_pipe_sequence));
 	}
 	return (ret_pipe_sequence);
 }
