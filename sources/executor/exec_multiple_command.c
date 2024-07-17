05d478f23a1df2d09f32ac0276d821ba83df0287
Daewon Min <damin@c10r3s13.42seoul.kr>
2024-07-17 23:11:39 +0900

    fix: redirect

--- sources/executor/exec_multiple_command.c
+++ sources/executor/exec_multiple_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_multiple_command.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seonseo <seonseo@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 03:58:30 by seonseo           #+#    #+#             */
-/*   Updated: 2024/07/17 22:35:22 by seonseo          ###   ########.fr       */
+/*   Updated: 2024/07/17 23:10:30 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,10 +65,7 @@ int	wait_for_all_commands(size_t cmd_cnt, int *status, int *is_signaled)
 		if (wait(status) == -1)
 			return (-1);
 		if (WIFSIGNALED(*status))
-		{
-			printf("debug: %d\n", WTERMSIG(*status));
 			*is_signaled = 1;
-		}
 		cmd_cnt--;
 	}
 	return (0);
