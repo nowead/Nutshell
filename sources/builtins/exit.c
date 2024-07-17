7eb1348cb6f8fef59e86f9dd5bf36da197053bde
Daewon Min <damin@c10r3s13.42seoul.kr>
2024-07-17 20:37:26 +0900

    feat: Display error message when exit has arguments or non-numeric values

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,27 +6,45 @@
 /*   By: damin <damin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 14:00:48 by damin             #+#    #+#             */
-/*   Updated: 2024/07/11 16:02:04 by damin            ###   ########.fr       */
+/*   Updated: 2024/07/17 20:33:41 by damin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	no_numeric_err_exit(char **argv)
+{
+	ft_dprintf(2, \
+	"exit\nNutshell: exit: %s: numeric argument required\n", \
+	argv[1]);
+	exit(255);
+}
+
 void	exec_exit(char **argv, t_shell_ctx *shell_ctx)
 {
-	printf("\033[u\033[1B\033[1A");
-	printf("\nexit\n");
 	restore_echoctl(&(shell_ctx->old_term));
-	if (argv[1])
-		exit(ft_atoi(argv[1]));
-	else
-		exit(EXIT_SUCCESS);
+	exec_exit_in_process(argv);
 }
 
 void	exec_exit_in_process(char **argv)
 {
+	int	i;
+
+	i = 0;
 	if (argv[1])
-		exit(ft_atoi(argv[1]));
-	else
-		exit(EXIT_SUCCESS);
+	{
+		while (argv[1][i])
+		{
+			if (!ft_isdigit(argv[1][i]))
+				no_numeric_err_exit(argv);
+			i++;
+		}
+		if (argv[2])
+			err_exit("exit\nNutshell: exit: too many arguments", 0, 1);
+		if (ft_atoi(argv[1]) < 0)
+			no_numeric_err_exit(argv);
+		else
+			exit(ft_atoi(argv[1]));
+	}
+	exit(EXIT_SUCCESS);
 }
